63d4790f23cc9651b5442d507857444c51aee743
Omar Ahmed Abdel Hameed <omar.ahmed.nes6@gmail.com>
2026-02-25 23:01:38 +0200

    Refactor job processing algorithm: rearrange struct definitions and improve sorting logic

--- Task07/job_processing.cpp
+++ Task07/job_processing.cpp
@@ -9,24 +9,28 @@ Each job has processing time t[i] and penalty p[i] if finished after deadline d[
 Minimize total penalty.
 Solve this problem using Dynamic Programming, Brute Force and divide and conquer. 
 */
-int main()
-{
-    
-}
 // BruteForce solution
 struct Job
 {
     int id;
-    int deadline;
     int time;
+    int deadline;
     int penalty;
-};
 
-void jobProcessing(vector<Job> jobs)
+};
+ bool compareJobs(const Job& a, const Job& b)
+{
+    return a.id < b.id; 
+}
+struct Result {
+    std::vector<Job> bestOrder;
+    int totalPenalty;
+};
+Result jobProcessing(vector<Job> jobs)
 {
     int minPenalty = INT_MAX;
     vector<Job> bestOrder;
-    sort(jobs.begin(), jobs.end());
+    sort(jobs.begin(), jobs.end(),compareJobs);
     vector<Job> result;
     do
     {
@@ -45,8 +49,25 @@ void jobProcessing(vector<Job> jobs)
         bestOrder = jobs;
     }
         
-    } while (next_permutation(jobs.begin(),jobs.end()));
+    } while (next_permutation(jobs.begin(),jobs.end(),compareJobs));
     
-
+return {bestOrder,minPenalty};
 }
 
+
+    int main()
+    {
+        //----------------------------Brute Force--------------------------------------
+        vector<Job> jobs = {
+            {1,4,5,10},
+            {2,2,6,20},
+            {3,1,6,30}
+        };
+        auto sortedJobSequence = jobProcessing(jobs);
+        for(auto job : sortedJobSequence.bestOrder)
+        {
+            cout << "Job " << job.id << " (Penalty: " << job.penalty << ")\n";
+        
+        }
+        std::cout << "Total penalty: " << sortedJobSequence.totalPenalty << "\n";
+    }
