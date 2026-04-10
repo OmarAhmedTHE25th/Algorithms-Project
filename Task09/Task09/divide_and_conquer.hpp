fb3a48f9c0f5aa1c840432ce450400915d11953e
Ahmed El Henawy <ahmedelhenawy03@gmail.com>
2026-04-10 18:15:59 +0200

    Task09 K-Clustering implemented

---
+++ Task09/Task09/divide_and_conquer.hpp
@@ -0,0 +1,14 @@
+#ifndef DIVIDE_AND_CONQUER_H
+#define DIVIDE_AND_CONQUER_H
+
+#include <vector>
+#include "point.h"
+#include "clustering_common.h"
+
+<<<<<<< HEAD
+=======
+
+>>>>>>> 0f9d90a (Task09: Move completed clustering files into Task09/ folder)
+ClusterResult solveDivideAndConquer(const std::vector<Point>& points, int k);
+
+#endif
---
+++ tasks_assignment.md
@@ -0,0 +1,21 @@
+# Task Assignments
+
+Mohamed A Nady → Task 1
+
+Ahmed Mohamed → Task 2
+
+Rodina Mohamed → Task 3
+
+Ahmed Mohamed Abd-Elmageed → Task 4
+
+01032852050 → Task 5
+
+Hamza Mohamed → Task 6
+
+Ahmed Mohamed El Henawy → Task 9
+
+Shahdd → Task 8
+
+Omar Ahmed → Task 7
+
+Abdelaziz → Task 10
