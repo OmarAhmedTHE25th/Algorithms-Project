3c6cc6ea8fd5d5939fc1901bbf9b3384b9048ed3
Ahmed El Henawy <ahmedelhenawy03@gmail.com>
2026-04-10 18:00:47 +0200

    Add Divide and Conquer clustering

---
+++ divide_and_conquer.hpp
@@ -0,0 +1,11 @@
+#ifndef DIVIDE_AND_CONQUER_H
+#define DIVIDE_AND_CONQUER_H
+
+#include <vector>
+#include "point.hpp"
+#include "clustering_common.hpp"
+
+
+ClusterResult solveDivideAndConquer(const std::vector<Point>& points, int k);
+
+#endif
