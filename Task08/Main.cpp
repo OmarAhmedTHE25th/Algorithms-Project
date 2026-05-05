0bd72f2a6d758a738db3be5e47a4855a55eac512
shah44 <basilali20207@gmail.com>
2026-05-05 17:27:38 +0300

    Add files via upload

--- Task08/Main.cpp
+++ Task08/Main.cpp
@@ -3,28 +3,57 @@
 #include "Graph.h"
 #include "MinCutSolver.h"
 
-int main() {
-    int n;
-    std::cout << "Enter number of vertices: ";
-    std::cin >> n;
+using namespace std;
 
-    // Create Graph object
+int main() {
+    int n, choice;
+
+    cout << "==================================" << endl;
+    cout << "   CSE245 Task 8 - Min Cut UI" << endl;
+    cout << "==================================" << endl;
+    cout << "Enter number of vertices: ";
+    cin >> n;
+
     Graph g(n);
     g.inputGraph();
-    g.displayGraph();
 
-    // Create Solver object
     MinCutSolver solver(g);
 
-    cout << "  Brute Force Result" << endl;
-    cout << "Minimum Cut = " << solver.bruteForceMinCut() << endl;
+    do {
+        cout << "\n========= MENU =========" << endl;
+        cout << "1. Display Graph" << endl;
+        cout << "2. Brute Force Minimum Cut" << endl;
+        cout << "3. Iterative Improvement" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Choose: ";
+        cin >> choice;
 
-    cout << " Iterative Improvement Result" << endl;
-    cout << "Approximate Minimum Cut = "
-        << solver.iterativeImprovement() << endl;
+        switch (choice) {
+        case 1:
+            g.displayGraph();
+            break;
 
-    // Test a sample cut
-    solver.testCut();
+        case 2:
+            cout << "\nBrute Force Result" << endl;
+            cout << "Minimum Cut = "
+                << solver.bruteForceMinCut() << endl;
+            break;
+
+        case 3:
+            cout << "\nIterative Improvement Result" << endl;
+            cout << "Approximate Minimum Cut = "
+                << solver.iterativeImprovement() << endl;
+            break;
+
+        case 4:
+            cout << "Exiting program..." << endl;
+            break;
+
+        default:
+            cout << "Invalid choice!" << endl;
+        }
+
+    } while (choice != 4);
 
     return 0;
 }
